006fa763a7a822b0b00abe9be69b0ce99b80ba83
Ernesto Morell <emorell85@gmail.com>
2024-04-24 19:34:30 +0300

    hope understood it right about the roles this time #2

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
@@ -0,0 +1,26 @@
+package ru.kata.spring.boot_security.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.kata.spring.boot_security.demo.services.UserService;
+
+import java.security.Principal;
+
+@Controller
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping(value = "/user")
+    public String showUserInfo(Model model, Principal principal) {
+        model.addAttribute("user", userService.getUserByUsername(principal.getName()));
+        return "user-info";
+    }
+}
